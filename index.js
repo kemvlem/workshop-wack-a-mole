1c43be2bca2c7fa05c1b388cbeba18342f7bc609
kemvlem <asitimbaykemlem@gmail.com>
2020-07-22 22:05:40 -0400

    still working

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ var scoreDisplay = document.getElementById('score');
 let hole= document.getElementsByClassName("hole")
 console.log(scoreDisplay);
 console.log("hello")
+
+ 
+ function moles(){
+   Math.random() * (9 - 1 + 1)  << 0
+ }
+
+ setInterval(moles,1000)
+
+
+ hole[moles].classList.toggle('mole') 
